d121a24a5abba46cc4b5d5120464c15fa0a1b217
tenred <sheriefwissa@magneto-3.local>
2017-03-21 10:59:22 +1100

    Initial Commit

---
+++ Amuesement Park Generator1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amuesement Park Generator1
+//
+//  Created by Sherief Wissa on 21/3/17.
+//  Copyright © 2017 10 Red Hacks Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Amuesement Park Generator1Tests/Amuesement_Park_Generator1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Amuesement_Park_Generator1Tests.swift
+//  Amuesement Park Generator1Tests
+//
+//  Created by Sherief Wissa on 21/3/17.
+//  Copyright © 2017 10 Red Hacks Pty Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import Amuesement_Park_Generator1
+
+class Amuesement_Park_Generator1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
