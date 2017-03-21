58120d402704345dc7a9cf10ac8e9f1c4fd55742
tenred <sheriefwissa@magneto-3.local>
2017-03-21 11:05:14 +1100

    - Creation of Revision Project Files2 in New Branch

---
+++ structEntrantProfileInformation.swift
@@ -0,0 +1,9 @@
+//
+//  structEntrantProfileInformation.swift
+//  Amuesement Park Generator1
+//
+//  Created by Sherief Wissa on 21/3/17.
+//  Copyright © 2017 10 Red Hacks Pty Ltd. All rights reserved.
+//
+
+import Foundation
